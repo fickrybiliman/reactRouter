5b64bab5b109ca43253eaf13c94c9490588914f4
fickrybiliman <bil.iman.fickry@gmail.com>
2019-01-22 13:57:07 +0700

    adda url parameter sample

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # reactRouter
 
 ### Pindah dari satu halaman ke halaman lain dengan single page application
-#### * npm install react-router-dom
+#### * npm install react-router-dom;
+#### * import { BrowserRouter as Router, Route, Link } from "react-router-dom";
--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import Table from './components/Table';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
 
+const Index = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
+const Users = (props) => {
+  // console.log(props)
+  return (
+    <h2>Users With ID : { props.match.params.id } , Nama : { props.match.params.nama }</h2>
+  )
+}
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      siswa : [
-        {id: 1, nama: 'Fickry Bil Iman', kelas: '7A'},
-        {id: 2, nama: 'Keanu Reeves', kelas: '8B'},
-        {id: 3, nama: 'Dian Sastro', kelas: '3C'},
-      ],
-      judul : 'Hello World!!'
-    }
-  }
-
   render() {
     return (
-      <div>
-        <h1>List Data Siswa</h1>
-        <Table 
-          siswa = { this.state.siswa } 
-          judulBaru = {this.state.judul }
-        />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to = "/">Home</Link>
+              </li>
+              <li>
+                <Link to = "/about">About</Link>
+              </li>
+              <li>
+                <Link to = "/users/1/andi">Users 1</Link>
+                <Link to = "/users/2/budi">Users 2</Link>
+                <Link to = "/users/3/anto">Users 3</Link>
+                <Link to = "/users/4/safi">Users 4</Link>
+                <Link to = "/users/5/riki">Users 5</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path = '/' exact component = { Index } />
+          <Route path = '/about' component = { About } />
+          <Route path = '/users/:id/:nama' component = { Users} />
+        </div>
+      </Router>
     )
   }
 }
